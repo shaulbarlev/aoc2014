df1d3f98282a3772c03f14deb661c4cbcb3279d3
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2024-12-11 19:18:29 +0200

    kind of working but there's a bug maybe confusing someplace x and y

--- 6/6.js
+++ 6/6.js
@@ -2,7 +2,8 @@ const input = fs.readFile('6/example.txt', 'utf-8', (err, input) => {
     let map = input.split(/\n/)
     map = map.map(line => line.split(''))
     console.log(map)
-
+    let i = 0
+    let atEdge = false
     const next = (mapInput) => {
         //determine guard position
         let pos
@@ -13,23 +14,56 @@ const input = fs.readFile('6/example.txt', 'utf-8', (err, input) => {
                 break
             }
         }
-        // return pos
+        const x = pos[0]
+        const y = pos[1]
+        const direction = pos[2]
+        let newDirection
+        const getFrontPos = (posInput) => {
+            switch (posInput[2]) {
+                case '^':
+                    newDirection = '>'
+                    return [posInput[0], posInput[1] - 1]
+                case '>':
+                    newDirection = 'V'
+                    return [posInput[0] + 1, posInput[1]]
+                case 'V':
+                    newDirection = '<'
+                    return [posInput[0], posInput[1] + 1]
+                case '<':
+                    newDirection = '^'
+                    return [posInput[0] - 1, posInput[1]]
+            }
+        }
+
+        const frontX = getFrontPos(pos)[0]
+        const frontY = getFrontPos(pos)[1]
+        //check if guard is at the edge.
+        if (frontY > 8) atEdge = true
+
+
+        const advance = (directionInput) => {
+            map[y][x] = 'X'
+            map[frontY][frontX] = pos[2]
+        }
 
         //determine if in front of guard there's an obstacle
-        switch (pos[2]) {
-            case '^':
-                
-                break
-            case 'V':
-                break
-            case '<':
-                break
-            case '>':
-                break
+        if (map[frontY][frontX] == '#') {
+            //rotate!
+            map[y][x] = newDirection
         }
+        else { //free to continue
+            advance(direction)
+        }
+        return map
+    }
+
+    while (!atEdge) {
+        next(map)
+        i++
     }
 
     console.log(
-        next(map)
+        next(map),i
     )
+
 })
