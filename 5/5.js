31f1875c7159ed6936bf711aa25441404a187495
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2024-12-11 14:01:26 +0200

    checkOrder functional

--- 5/5.js
+++ 5/5.js
@@ -1,12 +1,33 @@
-const input = fs.readFile('5/input.txt', 'utf-8', (err, input) => {
-    let [rules,updates] = [input.split(/\n\n/)[0],input.split(/\n\n/)[1]]
+const input = fs.readFile('5/example.txt', 'utf-8', (err, input) => {
+    let [rules, updates] = [input.split(/\n\n/)[0], input.split(/\n\n/)[1]]
     rules = rules.split(/\n/)
     rules = rules.map(rule => rule.split('|'))
     // console.log(rules)
-    
+
     updates = updates.split(/\n/)
     updates = updates.map(update => update.split(','))
-    console.log(updates)
+    console.log(rules[0])
 
+    const checkOrder = (update) => {
+        // iterating over all the rules.
+        for (const rule in rules) {
+            // checking if the update contains both numbers in the current rule.
+            let indexOfFirstValue = update.indexOf(rules[rule][0])
+            let indexOfSecondValue = update.indexOf(rules[rule][1])
+            if (indexOfFirstValue != -1 && indexOfSecondValue != -1) {
+                console.log(indexOfFirstValue)
+                console.log(indexOfSecondValue)
+                if (indexOfSecondValue < indexOfFirstValue) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
 
+    console.log(updates[0])
+
+    console.log(
+        checkOrder(updates[5])
+    )
 })
